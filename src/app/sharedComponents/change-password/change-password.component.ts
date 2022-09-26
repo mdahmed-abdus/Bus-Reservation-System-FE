fa6984502e3c0568b9fa9d1629f76bc0a08785aa
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2022-09-26 17:15:22 +0530

    Add change password form

--- src/app/sharedComponents/change-password/change-password.component.ts
+++ src/app/sharedComponents/change-password/change-password.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css'],
 })
 export class ChangePasswordComponent implements OnInit {
+  oldPwd: string;
+  newPwd: string;
+  confirmNewPwd: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.oldPwd && this.newPwd && this.confirmNewPwd) {
+      alert('Submitting');
+      // TODO: submit to backend
+    } else {
+      alert('Please enter all fields');
+    }
+  }
 }
